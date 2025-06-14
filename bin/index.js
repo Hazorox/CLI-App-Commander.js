6ff85c2c31572367f3b0c8844d4697a5b9082254
Hamza Mohammed <hamzaawad8899@gmail.com>
2025-06-14 19:26:34 +0300

    added SHENGA YEAH , cool name :P

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,47 @@
+#!/usr/bin/env node
+
 import { Command } from "commander";
+import figlet from "figlet";
 const app = new Command();
+
 app
   .name("JScripting")
   .description(
     "A random app made by hazorox ( On GitHub ) for educational purposes"
   )
-  .argument('<string...>')
   .version("1.0.0 Alpha")
-  .option("-n", "Let us know your name :>","Weird Ah User")
-  .action((args, option)=>{
-    if (option.n){
-        console.log(`Welcome!\nWhat an interesting name, ${args[0]}\nHope you enjoy your try!`)
+  .command("setUser")
+  .description("Let us know your name or else...")
+  .argument("<string...>")
+  .option("-n", `Try writing "huh"`, "Weird Ah User")
+  .action((args, option) => {
+    let name = args[0];
+    if (name.toUpperCase() == "HUH") {
+      console.log("\n\n\n");
+      figlet.text(
+        "GET OUT >:O",
+        {
+          font: "Star Wars",
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 120,
+          whitespaceBreak: true,
+        },
+        function (err, data) {
+          if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+          }
+          console.log(data);
+        }
+      );
+      name = "ↈ";
+    } else {
+      console.log(
+        `Welcome!\nWhat an interesting name, ${name}\nHope you enjoy your try!`
+      );
     }
-  })
-app.parse()
+    const userName = name;
+  });
+app.parse();
